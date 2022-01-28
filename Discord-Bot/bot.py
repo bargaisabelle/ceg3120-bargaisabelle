899164b9bfa85d73e3d7b575e6ce8dc3239b301c
Ubuntu <bargaisabelle@gmail.com>
2022-01-28 04:07:10 +0000

    adding pic/quotes to bot.py

--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -29,25 +29,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
+    movie_quotes = [
+        'I dont even have any good skills. You know like nunchuck skills, bow hunting skills, computer hacking skills.',    
+        'I am not a computer nerd. I prefer to be called a hacker',
+        'Its still magic even if you know how its done',
+        'Theres a new virus in the database, its replicating, eating up memory. What do I do? Type cookie you idiot',
     ]
 
-    hitchhiker_quotes = [
-        'There is an art, it says, or rather, a knack to flying. The knack lies in learning how to throw yourself at the ground and miss.',
-        'It is a mistake to think you can solve any major problems just with potatoes.',
-        'In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.',
-        'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.',
-    ]
-
-    if message.content == 'towel!':
-        #response = random.choice(brooklyn_99_quotes)
-        response = random.choice(hitchhiker_quotes)
+    if message.content == 'hacker':
+        response = random.choice(movie_quotes)
         await message.channel.send(response)
+        await message.channel.send(file=discord.File('intro-1596024186.jpg'))
 
 client.run(TOKEN)
